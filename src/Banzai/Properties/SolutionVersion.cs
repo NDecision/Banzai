2b4b840f6625ecbc316be14069f9926bb4f9f04f
Eric Swann <eric_swann@volusion.com>
2014-10-03 10:16:45 -0500

    Fixed up the solution version

--- src/Banzai/Properties/SolutionVersion.cs
+++ src/Banzai/Properties/SolutionVersion.cs
@@ -4,9 +4,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Banzai")]
 [assembly: AssemblyCopyright("Copyright 2014 Eric Swann - All rights reserved.")]
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
 
-[assembly: AssemblyInformationalVersion("1.0.2.0")]
+[assembly: AssemblyInformationalVersion("1.0.3.0")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
