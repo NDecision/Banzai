6c24a2ee2010aa4f2b84b9ba76e9012e22be3717
Eric Swann <eric_swann@volusion.com>
2014-10-03 10:28:37 -0500

    Updated solution version

--- src/Banzai/Properties/SolutionVersion.cs
+++ src/Banzai/Properties/SolutionVersion.cs
@@ -4,9 +4,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Banzai")]
 [assembly: AssemblyCopyright("Copyright 2014 Eric Swann - All rights reserved.")]
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.4")]
+[assembly: AssemblyFileVersion("1.0.4")]
 
-[assembly: AssemblyInformationalVersion("1.0.3.0")]
+[assembly: AssemblyInformationalVersion("1.0.4.0")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
