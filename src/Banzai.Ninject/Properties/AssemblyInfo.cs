72b8221c9777e8ef81299af4208d6d2d8918aeda
Eric Swann <eric@ericswann.org>
2014-10-20 09:24:09 -0500

    Added the ninject nuspec

--- src/Banzai.Ninject/Properties/AssemblyInfo.cs
+++ src/Banzai.Ninject/Properties/AssemblyInfo.cs
@@ -1,26 +1,19 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Banzai.Ninject")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An easy Ninject extension for registering nodes in Banzai.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Banzai.Ninject")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ffa40ae7-f881-4c0f-b76a-6b63b161148d")]
+[assembly: Guid("c7f5b0dd-916c-47bc-9c52-65a91907d487")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +25,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
